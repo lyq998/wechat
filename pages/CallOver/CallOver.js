1b7185f7b4a8c1dbdcd1b4e46195241b5a20004b
lyq998 <lyq998@users.noreply.github.com>
2018-11-11 21:14:30 +0800

    2018/11/11
    完善about和introduction界面
    完善点名（callover）页面，但是任然没有达到最终目标

--- pages/CallOver/CallOver.js
+++ pages/CallOver/CallOver.js
@@ -1,13 +1,14 @@
 // pages/CallOver/CallOver.js
 Page({  
   data:{
+    random:1,
+    length:null,
     input:null,
     classmates: []
   },
   onLoad: function () {
     // 展示本地存储能力
-    this.setData({
-      classmates:wx.getStorageSync('classmates')})
+    this.updateClassmates()
   },
 
   resetbox: function (e) {
@@ -22,6 +23,35 @@ Page({
     var classmates = wx.getStorageSync('classmates') || []
     classmates.push(this.input)
     wx.setStorageSync('classmates', classmates)
+    wx.showToast({
+      title: '添加成功',
+    })
+    this.updateClassmates()
+  },
+  start:function(){
+    var length=this.data.length;
+    this.setData({
+      random: Math.floor(Math.random()*length)   
+    })
+  },
+  clear :function(){
+    wx.removeStorage({
+      key: 'classmates',
+      success: function(res) {
+        wx.showToast({
+          title: '清除成功',
+        })
+      },
+    });
+    this.updateClassmates()
+  },
+  updateClassmates:function(){
+    this.setData({
+      classmates: wx.getStorageSync('classmates')
+    });
+    this.setData({
+      length: this.data.classmates.length
+    })
   }
 })
 
